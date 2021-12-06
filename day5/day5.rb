3edc68bd61b2ced6afa00db888ec899872dd7bb7
Pascal Hurni <phi@pragmaticpurist.org>
2021-12-06 15:52:13 +0100

    Update day5 solution handling both parts

--- day5/day5.rb
+++ day5/day5.rb
@@ -1,4 +1,4 @@
-# AOC-2021 Day 5 Part 1
+# AOC-2021 Day 5 both parts
 
 class Map
   def initialize
@@ -17,11 +17,20 @@ class Map
     y_range.each { |y| draw_point(x, y) }
   end
 
+  def draw_diagonal_line(x, y, steps, x_delta, y_delta)
+    steps.times do
+      draw_point(x, y)
+      x += x_delta
+      y += y_delta
+    end
+  end
+
   def point_count_matching(&block)
     @storage.values.flatten.compact.count(&block)
   end
 end
 
+handle_diagonals = ARGV.delete('-2')
 segments = ARGF.readlines.map { |line| line.match(/(\d+),(\d+) -> (\d+),(\d+)/).captures.map(&:to_i) }
 
 map = segments.each_with_object(Map.new) do |segment, map|
@@ -29,6 +38,8 @@ map = segments.each_with_object(Map.new) do |segment, map|
     map.draw_vertical_line(segment[0], Range.new(*[segment[1], segment[3]].sort))
   elsif segment[1] == segment[3]
     map.draw_horizontal_line(Range.new(*[segment[0], segment[2]].sort), segment[1])
+  elsif handle_diagonals && (segment[0]-segment[2]).abs == (segment[1]-segment[3]).abs
+    map.draw_diagonal_line(segment[0], segment[1], (segment[0]-segment[2]).abs+1, segment[2] <=> segment[0], segment[3] <=> segment[1])
   end
 end
 
